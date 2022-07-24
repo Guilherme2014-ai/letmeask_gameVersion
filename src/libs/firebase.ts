78646c6da5ce0bd5ca8704cb9a7efef69082649f
Guilherme Henrique <guilhermehenrique78383@gmail.com>
2022-07-24 19:51:38 -0300

    Update firebase.ts

--- src/libs/firebase.ts
+++ src/libs/firebase.ts
@@ -4,14 +4,7 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
-const envData = {
-  apiKey: "AIzaSyBXkse2VupCATNEkzSaigRYWHsklFZz_A4",
-  authDomain: "letmeask-f5b3c.firebaseapp.com",
-  projectId: "letmeask-f5b3c",
-  storageBucket: "letmeask-f5b3c.appspot.com",
-  messagingSenderId: "1023281800946",
-  appId: "1:1023281800946:web:c0ca6e91a0baf3626a526b",
-};
+const envData = {"TOMA CUIDADO BIXO"};
 
 const firebaseConfig = envData;
 
